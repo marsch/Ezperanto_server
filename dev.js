75ea16a1a59748d79b27b0845cc325d0161d5bac
Mario Scheliga <mario@sourcegarden.com>
2011-03-19 14:57:45 +0100

    dependencies as references

--- dev.js
+++ dev.js
@@ -1,3 +1,6 @@
+require.paths.push('./deps/');
+require.paths.push('./deps/connect/support');
+require.paths.push('./deps/connect/middleware');
 global.applications = []
 applications['server'] = require('./includes/application');
 applications['server'].bootstrap(function () {
